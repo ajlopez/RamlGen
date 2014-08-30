9aef382350ecbc7cf32f899498fe0f5086fdf4b7
ajlopez <webmaster@ajlopez.com>
2014-08-30 14:48:09 -0300

    set name

--- source/ramlgen/libs/names.js
+++ source/ramlgen/libs/names.js
@@ -13,13 +13,30 @@ function getName(name) {
         
     var lastch = name[l - 1];
     
-    if (lastch == 's')
-        name = name.substring(0, l - 1);
+    if (lastch == 's') {
+        if (l > 3 && name[l - 3] == 'i' && name[l - 2] == 'e')
+            name = name.substring(0, l - 3) + 'y';
+        else
+            name = name.substring(0, l - 1);
+    }
         
     return name;
 }
 
+function getSetName(name) {
+    name = getName(name);
+    
+    if (name.length)
+        if (name[name.length - 1] == 'y')
+            name = name.substring(0, name.length - 1) + 'ies';
+        else if (name[name.length - 1] != 's')
+            name = name + 's';
+            
+    return name;
+}
+
 module.exports = {
-    getName: getName
+    getName: getName,
+    getSetName: getSetName
 }
 
--- test/names.js
+++ test/names.js
@@ -6,5 +6,13 @@ exports['get name'] = function (test) {
     test.equal(names.getName('songs'), 'song');
     test.equal(names.getName('/song'), 'song');
     test.equal(names.getName('/songs'), 'song');
+    test.equal(names.getName('/cities'), 'city');
 }
 
+exports['get set name'] = function (test) {
+    test.equal(names.getSetName('song'), 'songs');
+    test.equal(names.getSetName('songs'), 'songs');
+    test.equal(names.getSetName('/song'), 'songs');
+    test.equal(names.getSetName('/songs'), 'songs');
+    test.equal(names.getSetName('/city'), 'cities');
+}
